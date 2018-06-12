4e7016bbc70cab83078acb03ccec8c7c6cb16205
isaichenko <isaichenko.e@gmail.com>
2018-06-12 12:23:06 +0300

    Concern for Devise and Routs

---
+++ app/controllers/concerns/devise_whitelist.rb
@@ -0,0 +1,11 @@
+module DeviseWhitelist
+  extend ActiveSupport::Concern
+
+  included do
+    before_action :configure_permitted_parameters, if: :devise_controller?
+  end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:account_update, keys: [:company_name])
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
+
   resources :jobs
-  devise_for :users
+
   root to: "jobs#index"
 end
