bcad31b1e02345efeb895fe0626bc6112c1bd970
isaichenko <isaichenko.e@gmail.com>
2018-06-13 11:43:49 +0300

    facebook 12

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
-  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  get '/users/auth/facebook/callback', to: redirect('/accounts/auth/facebook/callback')
 
   resources :jobs
 
   resources :users, only: [:show]
 
   root to: "jobs#index"
+
+  devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' },
+  :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 end
