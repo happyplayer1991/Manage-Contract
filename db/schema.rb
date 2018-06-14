a9f67b548a30c32f302b1ffc09928ebde6a5aad0
isaichenko <isaichenko.e@gmail.com>
2018-06-15 01:59:45 +0300

    Tamplates

--- db/schema.rb
+++ db/schema.rb
@@ -88,14 +88,7 @@ ActiveRecord::Schema.define(version: 20180614075737) do
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
-    t.string "company_name"
-    t.integer "hires_per_year"
     t.string "name"
-    t.string "phone"
-    t.string "country"
-    t.string "city"
-    t.integer "postal_code"
-    t.text "payment_info"
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
--- db/seeds.rb
+++ db/seeds.rb
@@ -34,6 +34,6 @@ Job.create!(title: "Interesting", job_type_id: 3, job_area_id: 3, salary_min: 15
 Job.create!(title: "Super", job_type_id: 4, job_area_id: 4, salary_min: 1500, salary_max: 3500, hires: 1, description: "Some description", email: "seed@example.com", address: "Some Address", experience: 4, education_id: 4, user_id: 2)
 Job.create!(title: "Nice", job_type_id: 5, job_area_id: 5, salary_min: 4500, salary_max: 5000, hires: 3, description: "Some description", email: "seed@example.com", address: "Some Address", experience: 1, education_id: 5, user_id: 2)
 
-Company.create!(title: "Amazon", user_id: 1)
-Company.create!(title: "Google", user_id: 2)
-Company.create!(title: "NBA", user_id: 3)
+Company.create!(title: "Amazon", hires_per_year: 5, name: "Bill", phone: "1234567899", country: "USA", city: "Boston", postal_code: "12345", payment_info: "PayPal", about: "We are Great company", why_join_us: "Because we Great!", website: "google.com", industry: "Some Industry", number_of_employee: 150, user_id: 1)
+Company.create!(title: "Google", hires_per_year: 15, name: "Smith", phone: "1234567899", country: "Canada", city: "Toronto", postal_code: "12345", payment_info: "PayPal", about: "We are Great company", why_join_us: "Because we Great!", website: "camp.com", industry: "Some Industry", number_of_employee: 50, user_id: 2)
+Company.create!(title: "NBA", hires_per_year: 25, name: "Jack", phone: "1234567899", country: "Some Coutry", city: "Some City", postal_code: "12345", payment_info: "PayPal", about: "We are Great company", why_join_us: "Because we Great!", website: "bong.com", industry: "Some Industry", number_of_employee: 15, user_id: 3)
